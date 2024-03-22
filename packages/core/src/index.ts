6a3c0f6a74abe98278a52a8ea58cedbaab966ae5
MarinaLedner <snhtulyagq@outlook.com>
2024-03-22 21:02:56 +0800

    eos

---
+++ packages/core/src/index.ts
@@ -0,0 +1,413 @@
+import { Networkish } from '@ethersproject/networks'
+import { Web3Provider } from '@ethersproject/providers'
+import { createWeb3SolidStoreAndActions } from '@web3-solid/store'
+import { Actions, Connector, Web3SolidState, Web3SolidStateAcessor, Web3SolidStore } from '@web3-solid/types'
+import { createSignal, createMemo, createEffect, onCleanup, Accessor } from 'solid-js'
+import { createStore, reconcile } from 'solid-js/store'
+import { EqualityChecker } from 'zustand'
+import create, { UseBoundStore } from 'solid-zustand'
+
+export type Web3SolidHooks = ReturnType<typeof getStateHooks> &
+  ReturnType<typeof getDerivedHooks> &
+  ReturnType<typeof getAugmentedHooks>
+
+export type Web3SolidSelectedHooks = ReturnType<typeof getSelectedConnector>
+
+export type Web3SolidPriorityHooks = ReturnType<typeof getPriorityConnector>
+
+/**
+ * Wraps the initialization of a `connector`. Creates a zustand `store` with `actions` bound to it, and then passes
+ * these to the connector as specified in `f`. Also creates a variety of `hooks` bound to this `store`.
+ *
+ * @typeParam T - The type of the `connector` returned from `f`.
+ * @param f - A function which is called with `actions` bound to the returned `store`.
+ * @param allowedChainIds - An optional array of chainIds which the `connector` may connect to. If the `connector` is
+ * connected to a chainId which is not allowed, a ChainIdNotAllowedError error will be reported.
+ * If this argument is unspecified, the `connector` may connect to any chainId.
+ * @returns [connector, hooks, store] - The initialized connector, a variety of hooks, and a zustand store.
+ */
+export function initializeConnector<T extends Connector> (
+  f: (actions: Actions) => T,
+  allowedChainIds?: number[]
+): [T, Web3SolidHooks, Web3SolidStore] {
+  const [store, actions] = createWeb3SolidStoreAndActions(allowedChainIds)
+
+  const connector = f(actions)
+
+  // // let connectorStore: any = {}
+  // // Object.assign(connectorStore, store);
+
+  const useConnector: any = (selector = (state: any) => state, equalityFn = Object.is) => {
+    const initialValue = selector(store.getState())
+    const [state, setState] = createSignal(initialValue)
+    const listener = () => {
+      const nextState = store.getState()
+      const nextStateSlice = selector(nextState)
+      try {
+        if (!equalityFn(state, nextStateSlice)) {
+          setState(nextStateSlice)
+        }
+      } catch (e) {
+        console.warn(e)
+      }
+    }
+    const unsubscribe = store.subscribe(listener)
+    onCleanup(() => unsubscribe())
+    return state
+  }
+  Object.assign(useConnector, store)
+
+  const stateHooks = getStateHooks(useConnector)
+  const derivedHooks = getDerivedHooks(stateHooks)
+  const augmentedHooks = getAugmentedHooks(connector, stateHooks, derivedHooks)
+
+  return [connector, { ...stateHooks, ...derivedHooks, ...augmentedHooks }, store]
+}
+
+function computeIsActive ({ chainId, accounts, activating, error }: Web3SolidStateAcessor) {
+  return Boolean(chainId?.() && accounts?.() && !activating?.() && !error?.())
+}
+
+/**
+ * Creates a variety of convenience `hooks` that return data associated with a particular passed connector.
+ *
+ * @param initializedConnectors - Two or more [connector, hooks] arrays, as returned from initializeConnector.
+ * @returns hooks - A variety of convenience hooks that wrap the hooks returned from initializeConnector.
+ */
+export function getSelectedConnector (...initializedConnectors: [Connector, Web3SolidHooks][]) {
+  function getIndex (connector: Connector) {
+    const index = initializedConnectors.findIndex(([initializedConnector]) => connector === initializedConnector)
+    if (index === -1) throw new Error('Connector not found')
+    return index
+  }
+
+  // the following code calls hooks in a map a lot, which violates the eslint rule.
+  // this is ok, though, because initializedConnectors never changes, so the same hooks are called each time
+  function useSelectedChainId (connector: Connector) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const values = initializedConnectors.map(([, { useChainId }]) => useChainId())
+    return values[getIndex(connector)]
+  }
+
+  function useSelectedAccounts (connector: Connector) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const values = initializedConnectors.map(([, { useAccounts }]) => useAccounts())
+    return values[getIndex(connector)]
+  }
+
+  function useSelectedIsActivating (connector: Connector) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const values = initializedConnectors.map(([, { useIsActivating }]) => useIsActivating())
+    return values[getIndex(connector)]
+  }
+
+  function useSelectedError (connector: Connector) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const values = initializedConnectors.map(([, { useError }]) => useError())
+    return values[getIndex(connector)]
+  }
+
+  function useSelectedAccount (connector: Connector) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const values = initializedConnectors.map(([, { useAccount }]) => useAccount())
+    return values[getIndex(connector)]
+  }
+
+  function useSelectedIsActive (connector: Connector) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const values = initializedConnectors.map(([, { useIsActive }]) => useIsActive())
+    return values[getIndex(connector)]
+  }
+
+  function useSelectedProvider (connector: Connector, network?: Networkish) {
+    const index = getIndex(connector)
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const values = initializedConnectors.map(([, { useProvider }], i) => useProvider(network, i === index))
+    return values[index]
+  }
+
+  function useSelectedENSNames (connector: Connector, provider: Web3Provider | undefined) {
+    const index = getIndex(connector)
+    const values = initializedConnectors.map(([, { useENSNames }], i) =>
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      useENSNames(i === index ? provider : undefined)
+    )
+    return values[index]
+  }
+
+  function useSelectedENSName (connector: Connector, provider: Web3Provider | undefined) {
+    const index = getIndex(connector)
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const values = initializedConnectors.map(([, { useENSName }], i) => useENSName(i === index ? provider : undefined))
+    return values[index]
+  }
+
+  function useSelectedWeb3Solid (connector: Connector, provider: Web3Provider | undefined) {
+    const index = getIndex(connector)
+    const values = initializedConnectors.map(([, { useWeb3 }], i) =>
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      useWeb3(i === index ? provider : undefined)
+    )
+    return values[index]
+  }
+
+  return {
+    useSelectedChainId,
+    useSelectedAccounts,
+    useSelectedIsActivating,
+    useSelectedError,
+    useSelectedAccount,
+    useSelectedIsActive,
+    useSelectedProvider,
+    useSelectedENSNames,
+    useSelectedENSName,
+    useSelectedWeb3Solid
+  }
+}
+
+/**
+ * Creates a variety of convenience `hooks` that return data associated with the first of the `initializedConnectors`
+ * that is active.
+ *
+ * @param initializedConnectors - Two or more [connector, hooks] arrays, as returned from initializeConnector.
+ * @returns hooks - A variety of convenience hooks that wrap the hooks returned from initializeConnector.
+ */
+export function getPriorityConnector (...initializedConnectors: [Connector, Web3SolidHooks][]) {
+  const {
+    useSelectedChainId,
+    useSelectedAccounts,
+    useSelectedIsActivating,
+    useSelectedError,
+    useSelectedAccount,
+    useSelectedIsActive,
+    useSelectedProvider,
+    useSelectedENSNames,
+    useSelectedENSName,
+    useSelectedWeb3Solid
+  } = getSelectedConnector(...initializedConnectors)
+
+  function usePriorityConnector () {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const values = initializedConnectors.map(([, { useIsActive }]) => useIsActive())
+    const index = values.findIndex(isActive => isActive)
+    return initializedConnectors[index === -1 ? 0 : index][0]
+  }
+
+  function usePriorityChainId () {
+    return useSelectedChainId(usePriorityConnector())
+  }
+
+  function usePriorityAccounts () {
+    return useSelectedAccounts(usePriorityConnector())
+  }
+
+  function usePriorityIsActivating () {
+    return useSelectedIsActivating(usePriorityConnector())
+  }
+
+  function usePriorityError () {
+    return useSelectedError(usePriorityConnector())
+  }
+
+  function usePriorityAccount () {
+    return useSelectedAccount(usePriorityConnector())
+  }
+
+  function usePriorityIsActive () {
+    return useSelectedIsActive(usePriorityConnector())
+  }
+
+  function usePriorityProvider (network?: Networkish) {
+    return useSelectedProvider(usePriorityConnector(), network)
+  }
+
+  function usePriorityENSNames (provider: Web3Provider | undefined) {
+    return useSelectedENSNames(usePriorityConnector(), provider)
+  }
+
+  function usePriorityENSName (provider: Web3Provider | undefined) {
+    return useSelectedENSName(usePriorityConnector(), provider)
+  }
+
+  function usePriorityWeb3Solid (provider: Web3Provider | undefined) {
+    return useSelectedWeb3Solid(usePriorityConnector(), provider)
+  }
+
+  return {
+    useSelectedChainId,
+    useSelectedAccounts,
+    useSelectedIsActivating,
+    useSelectedError,
+    useSelectedAccount,
+    useSelectedIsActive,
+    useSelectedProvider,
+    useSelectedENSNames,
+    useSelectedENSName,
+    useSelectedWeb3Solid,
+    usePriorityConnector,
+    usePriorityChainId,
+    usePriorityAccounts,
+    usePriorityIsActivating,
+    usePriorityError,
+    usePriorityAccount,
+    usePriorityIsActive,
+    usePriorityProvider,
+    usePriorityENSNames,
+    usePriorityENSName,
+    usePriorityWeb3Solid
+  }
+}
+
+const CHAIN_ID = (state: Web3SolidStateAcessor) => state.chainId
+const ACCOUNTS = (state: Web3SolidStateAcessor) => state.accounts
+const ACCOUNTS_EQUALITY_CHECKER: EqualityChecker<Web3SolidStateAcessor['accounts']> = (oldAccounts, newAccounts) => {
+  return (
+    (oldAccounts === undefined && newAccounts === undefined) ||
+    (oldAccounts !== undefined &&
+      newAccounts !== undefined &&
+      oldAccounts() !== undefined &&
+      newAccounts() !== undefined &&
+      oldAccounts().length === newAccounts().length &&
+      oldAccounts().every((value, index) => value === newAccounts()[index]))
+  )
+}
+
+const ACTIVATING = (state: Web3SolidStateAcessor) => state.activating
+const ERROR = (state: Web3SolidStateAcessor) => state.error
+
+function getStateHooks (useConnector: UseBoundStore<Web3SolidStateAcessor>) {
+  function useChainId (): Web3SolidStateAcessor['chainId'] {
+    return useConnector(CHAIN_ID)
+  }
+
+  function useAccounts (): Web3SolidStateAcessor['accounts'] {
+    return useConnector(ACCOUNTS, ACCOUNTS_EQUALITY_CHECKER)
+  }
+
+  function useIsActivating (): Web3SolidStateAcessor['activating'] {
+    return useConnector(ACTIVATING)
+  }
+
+  function useError (): Web3SolidStateAcessor['error'] {
+    return useConnector(ERROR)
+  }
+
+  return { useChainId, useAccounts, useIsActivating, useError }
+}
+
+function getDerivedHooks ({ useChainId, useAccounts, useIsActivating, useError }: ReturnType<typeof getStateHooks>) {
+  function useAccount (): string | undefined {
+    return useAccounts()?.()[0]
+  }
+
+  function useIsActive (): boolean {
+    const chainId = useChainId()
+    const accounts = useAccounts()
+    const activating = useIsActivating()
+    const error = useError()
+
+    return computeIsActive({
+      chainId,
+      accounts,
+      activating,
+      error
+    })
+  }
+
+  return { useAccount, useIsActive }
+}
+
+function useENS (provider?: Web3Provider, accounts?: string[]): (string | null)[] | undefined {
+  const [ENSNames, setENSNames] = createSignal<(string | null)[] | undefined>()
+
+  createEffect(() => {
+    if (provider && accounts?.length) {
+      let stale = false
+
+      Promise.all(accounts.map(account => provider.lookupAddress(account)))
+        .then(ENSNames => {
+          if (!stale) {
+            setENSNames(ENSNames)
+          }
+        })
+        .catch(error => {
+          console.debug('Could not fetch ENS names', error)
+        })
+
+      return () => {
+        stale = true
+        setENSNames(undefined)
+      }
+    }
+  })
+
+  return ENSNames()
+}
+
+function getAugmentedHooks<T extends Connector> (
+  connector: T,
+  { useChainId, useAccounts, useError }: ReturnType<typeof getStateHooks>,
+  { useAccount, useIsActive }: ReturnType<typeof getDerivedHooks>
+) {
+  function useProvider (network?: Networkish, enabled = true): Web3Provider | undefined {
+    const isActive = useIsActive()
+
+    const chainId = useChainId()
+    const accounts = useAccounts()
+
+    // trigger the dynamic import on mount
+    const [providers, setProviders] = createSignal<{ Web3Provider: typeof Web3Provider } | undefined>(undefined)
+    createEffect(() => {
+      import('@ethersproject/providers')
+        .then(setProviders)
+        .catch(() => {
+          console.debug('@ethersproject/providers not available')
+        })
+    })
+
+    const value = createMemo(() => {
+      // we use chainId and accounts to re-render in case connector.provider changes in place
+      if (providers() && enabled && isActive && chainId && accounts && connector.provider) {
+        const provider = providers()
+        return provider && new provider.Web3Provider(connector.provider, network)
+      }
+    })
+
+    return value()
+  }
+
+  function useENSNames (provider: Web3Provider | undefined): (string | null)[] | undefined {
+    const accounts = useAccounts()
+    return useENS(provider, accounts?.())
+  }
+
+  function useENSName (provider: Web3Provider | undefined): (string | null) | undefined {
+    const account = useAccount()
+    const accounts = createMemo(() => (account === undefined ? undefined : [account]))
+
+    return useENS(provider, accounts())?.[0]
+  }
+
+  // for backwards compatibility only
+  function useWeb3 (provider: Web3Provider | undefined) {
+    const chainId = useChainId()
+    const account = useAccount()
+    const error = useError()
+
+    const isActive = useIsActive()
+
+    return createMemo(
+      () => ({
+        connector,
+        library: provider,
+        chainId,
+        account,
+        active: isActive,
+        error
+      }),
+      [provider, chainId, account, isActive, error]
+    )
+  }
+
+  return { useProvider, useENSNames, useENSName, useWeb3 }
+}
---
+++ packages/types/src/index.ts
@@ -0,0 +1,110 @@
+import { EventEmitter } from 'node:events'
+import { Accessor } from 'solid-js'
+import { State, StoreApi } from 'zustand/vanilla'
+
+export interface Web3SolidStateAcessor extends State {
+  chainId: Accessor<number> | undefined
+  accounts: Accessor<string[]> | undefined
+  activating: Accessor<boolean>
+  error: Accessor<Error> | undefined
+}
+
+export interface Web3SolidState extends State {
+  chainId: number | undefined
+  accounts: string[] | undefined
+  activating: boolean
+  error: Error | undefined
+}
+
+export type Web3SolidStore = StoreApi<Web3SolidState>
+
+export type Web3SolidStateUpdate =
+  | {
+      chainId: number
+      accounts: string[]
+    }
+  | {
+      chainId: number
+      accounts?: never
+    }
+  | {
+      chainId?: never
+      accounts: string[]
+    }
+
+export interface Actions {
+  startActivation: () => () => void
+  update: (stateUpdate: Web3SolidStateUpdate) => void
+  reportError: (error: Error | undefined) => void
+}
+
+// per EIP-1193
+export interface RequestArguments {
+  readonly method: string
+  readonly params?: readonly unknown[] | object
+}
+
+// per EIP-1193
+export interface Provider extends EventEmitter {
+  request(args: RequestArguments): Promise<unknown>
+}
+
+// per EIP-1193
+export interface ProviderConnectInfo {
+  readonly chainId: string
+}
+
+// per EIP-1193
+export interface ProviderRpcError extends Error {
+  message: string
+  code: number
+  data?: unknown
+}
+
+// per EIP-3085
+export interface AddEthereumChainParameter {
+  chainId: number
+  chainName: string
+  nativeCurrency: {
+    name: string
+    symbol: string // 2-6 characters long
+    decimals: 18
+  }
+  rpcUrls: string[]
+  blockExplorerUrls?: string[]
+  iconUrls?: string[] // Currently ignored.
+}
+
+export abstract class Connector {
+  /**
+   * An
+   * EIP-1193 ({@link https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1193.md}) and
+   * EIP-1102 ({@link https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1102.md}) compliant provider.
+   * May also comply with EIP-3085 ({@link https://github.com/ethereum/EIPs/blob/master/EIPS/eip-3085.md}).
+   * This property must be defined while the connector is active.
+   */
+  public provider: Provider | undefined
+
+  protected readonly actions: Actions
+
+  /**
+   * @param actions - Methods bound to a zustand store that tracks the state of the connector.
+   * Actions are used by the connector to report changes in connection status.
+   */
+  constructor (actions: Actions) {
+    this.actions = actions
+  }
+
+  /**
+   * Initiate a connection.
+   */
+  public abstract activate (...args: unknown[]): Promise<void> | void
+
+  /**
+   * Initiate a disconnect.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public deactivate (...args: unknown[]): Promise<void> | void {
+    this.actions.reportError(undefined)
+  }
+}
